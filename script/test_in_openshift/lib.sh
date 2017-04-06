18b14a66d7b332305d26e7a0880e7030eccd909a
Anush Shetty <ashetty@redhat.com>
2017-04-06 12:45:59 +0530

    Display 'oc cluster up' output

--- script/test_in_openshift/lib.sh
+++ script/test_in_openshift/lib.sh
@@ -39,8 +39,7 @@ function install_oc_client () {
 
 function convert::oc_cluster_up () {
 
-    convert::run_cmd "oc cluster up"
-    exit_status=$?
+    oc cluster up; exit_status=$?
 
     if [ $exit_status -ne 0 ]; then
 	FAIL_MSGS=$FAIL_MSGS"exit status: $exit_status\n";
