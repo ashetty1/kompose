1ba035a17ccdc99d0cd8d0954b03fb5d60631039
Anush Shetty <ashetty@redhat.com>
2017-04-11 13:20:16 +0530

    Etherpad test

--- script/test_in_openshift/tests/etherpad.sh
+++ script/test_in_openshift/tests/etherpad.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,21 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Test case for buildconfig on kompose
+# Test case for kompose up/down with etherpad
 
 KOMPOSE_ROOT=$(readlink -f $(dirname "${BASH_SOURCE}")/../../..)
 source $KOMPOSE_ROOT/script/test/cmd/lib.sh
 source $KOMPOSE_ROOT/script/test_in_openshift/lib.sh
 
-convert::print_msg "Testing buildconfig on kompose"
+convert::print_msg "Testing kompose up/down with etherpad docker-compose file"
 
-docker_compose_file="${KOMPOSE_ROOT}/examples/buildconfig/docker-compose.yml"
+# Env variables for etherpad
+export $(cat ${KOMPOSE_ROOT}/script/test/fixtures/etherpad/envs)
+
+docker_compose_file="${KOMPOSE_ROOT}/script/test/fixtures/etherpad/docker-compose.yml"
 
 # Run kompose up
 convert::kompose_up $docker_compose_file
 
-# Check if the pods are up.
-convert::kompose_up_check -p foo
+# Check if the pods are up
+convert::kompose_up_check -p "etherpad mariadb"
 
 # Run Kompose down
 convert::kompose_down $docker_compose_file
